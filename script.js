10ee316f5a8ee0ae1472adc3d99f307a1fb25598
HaqpaHR <pereverzev.viacheslav@gmail.com>
2022-08-07 18:08:33 +0300

    hover board

---
+++ script.js
@@ -0,0 +1,33 @@
+const board = document.querySelector('#board');
+const SQUARES_NUMBER = 500;
+const color = ['yellow', 'green', 'red', 'blue', 'coral', 'salmon', 'fuchsia', 'rosybrown', 'orange']
+
+for (let i = 0; i < SQUARES_NUMBER; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => {
+        setColor(square)
+    })
+    square.addEventListener('mouseleave', () => {
+        setLeave(square)
+    })
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.backgroundColor = `${color}`;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function setLeave(element) {
+    element.style.backgroundColor = "#1d1d1d"
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random()*color.length);
+    return color[index]
+}
